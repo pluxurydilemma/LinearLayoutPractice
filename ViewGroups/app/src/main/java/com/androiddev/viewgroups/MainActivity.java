509514e73976239080b421100abd4c0fd4981b31
Abubakr Zakhidov <zakhidov.44@gmail.com>
2021-03-09 13:34:54 +0600

    First commit

---
+++ ViewGroups/app/src/main/java/com/androiddev/viewgroups/MainActivity.java
@@ -0,0 +1,25 @@
+package com.androiddev.viewgroups;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        View элемент
+//        ViewGroup - контейнер который имеет определенный характер
+//        выстраивания View элементов
+
+
+//        Что такое XML рахметка
+//        Liner Layout
+//        Relative Layout
+
+//        Constraint Layout
+
+    }
+}
